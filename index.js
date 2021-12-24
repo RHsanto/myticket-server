2bfab0a994ca70bf10f35a3af44cc4378c676121
Rakibul santo <SANTODON176@gmail.com>
2021-12-24 23:50:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+
+//midalware
+app.use(cors());
+app.use(express.json());  
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhafc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// 
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("allevents");
+    const allEventsCollection = database.collection("events");
+    const allOrdersCollection = database.collection("allOrders");
+   
+   
+      // GET API ORDERS
+      app.get('/all-events', async (req,res)=>{
+        const cursor = allEventsCollection.find({})
+        const events = await cursor.toArray();
+        res.send(events);
+  
+       });
+
+      // GET API ORDERS
+       app.get('/all-Orders', async(req,res)=>{
+         const cursor = allOrdersCollection.find({})
+         const orders = await cursor.toArray();
+         res.send(orders)
+        console.log(orders);
+       })
+      
+     
+       //  POST ORDERS
+       app.post('/addOrders', async(req,res)=>{
+         const orders = req.body;
+         const result = await allOrdersCollection.insertOne(orders);
+         res.send(result)
+         console.log(result);
+       })
+
+        // GET LOGGED USER ORDERS
+    app.get('/all-Orders/:email', async (req,res)=>{
+      const result = await allOrdersCollection.find({email: req.params.email}).toArray();
+      res.json(result)
+    })
+      
+       // GET SINGLE PRODUCT
+   app.get('/booking/:id', async (req,res)=>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const booking = await allEventsCollection.findOne(query)
+  res.json(booking);
+  });
+
+  //DELETE API ORDERS
+  app.delete('/all-Orders/:id', async(req,res)=>{
+    const id     = req.params.id;
+    const query  = {_id:ObjectId(id)} ;
+    const result = await allOrdersCollection.deleteOne(query)
+    res.json(result);
+   })
+ 
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+// 
+app.get('/', (req, res) => {
+  res.send('Hello World! MyTicket')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
